d2d2d895784b1c0a8186d179ce73ac123ead8c1d
kitamura <kitamura0070@gmail.com>
2020-06-29 21:41:31 +0900

    HTML

---
+++ main.js
@@ -0,0 +1,33 @@
+/*"use strict";
+{
+  //プロフィール読み込み
+  class Prof {
+    constructor(
+      HeaderImg,
+      IconImg,
+      Name,
+      Comment,
+      FollowNumber,
+      FollowerNumber
+    ) {
+      this.headerimg = HeaderImg;
+      this.iconimg = IconImg;
+      this.name = Name;
+      this.comment = Comment;
+      this.follownumber = FollowNumber;
+      this.followernumber = FollowerNumber;
+    }
+    show() {
+      document.write(`${this.headerimg}-${this.iconimg}`);
+    }
+  }
+
+  const Profs = [
+    new Prof(
+      "<img src='img/Prof/headimg.jpg'>",
+      "<img src='img/Prof/icon.jpg'>"
+    ),
+  ];
+  Profs[0].show();
+}
+*/
