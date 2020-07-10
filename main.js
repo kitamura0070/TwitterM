8db5b7877e0e633597485b26c0711a7e76c61cfa
kitamura <kitamura0070@gmail.com>
2020-07-11 04:27:09 +0900

    2週目

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-/*"use strict";
+"use strict";
+
 {
+  const ClickedFollow = document.querySelector("ClickedFollow");
+  const ClickedFollowing = document.getElementById("ClickedFollowing");
+  const Modal = document.getElementById("Modal");
+  const Mask = document.querySelector("Mask");
+  const Visible = document.querySelector("Visible");
+
+  ClickedFollowing.addEventListener("click", () => {
+    Modal.classList.add("Hidden");
+  });
+  // document.addEventListener("DOMContentLoaded", DA, false);
+
+  /*
   //プロフィール読み込み
   class Prof {
     constructor(
@@ -9,25 +22,26 @@
       Comment,
       FollowNumber,
       FollowerNumber
-    ) {
-      this.headerimg = HeaderImg;
-      this.iconimg = IconImg;
-      this.name = Name;
-      this.comment = Comment;
-      this.follownumber = FollowNumber;
-      this.followernumber = FollowerNumber;
+      ) {
+        this.headerimg = HeaderImg;
+        this.iconimg = IconImg;
+        this.name = Name;
+        this.comment = Comment;
+        this.follownumber = FollowNumber;
+        this.followernumber = FollowerNumber;
+      }
+      show() {
+        document.write(`${this.headerimg}-${this.iconimg}`);
+      }
     }
-    show() {
-      document.write(`${this.headerimg}-${this.iconimg}`);
-    }
-  }
-
-  const Profs = [
-    new Prof(
-      "<img src='img/Prof/headimg.jpg'>",
-      "<img src='img/Prof/icon.jpg'>"
-    ),
-  ];
-  Profs[0].show();
+    
+    const Profs = [
+      new Prof(
+        "<img src='img/Prof/headimg.jpg'>",
+        "<img src='img/Prof/icon.jpg'>"
+        ),
+      ];
+      Profs[0].show();
+    
+    */
 }
-*/
